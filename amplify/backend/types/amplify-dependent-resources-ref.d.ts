bf13d9469cd764ab05ac1230a05f11e64307094a
Gareth Pile <gareth.pile@leansystems.co.za>
2022-08-24 08:45:48 +0200

    Amplify BE init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "bestathletetest": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
