5cc2db3b5ae1d6752f2adf86c09d4db28c27dd4a
Gareth Pile <pile.gareth@gmail.com>
2022-08-31 15:01:23 +0200

    3rdParty update

--- src/ThreeSixtyDSL/ThirdParty.jsx
+++ src/ThreeSixtyDSL/ThirdParty.jsx
@@ -1,83 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./ThreeSixtyDSL.css";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import moment from "moment";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import PedalBikeIcon from "@mui/icons-material/PedalBike";
+
 import "antd/dist/antd.min.css";
-import { Avatar } from "antd";
-import { Card } from "antd";
-import Divider from "@mui/material/Divider";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+
 import { Row, Col } from "antd";
-import { Button, Tooltip } from "antd";
-import { EyeOutlined } from "@ant-design/icons";
-import PoolIcon from "@mui/icons-material/Pool";
-import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
-import { Auth, API, graphqlOperation } from "aws-amplify";
-import { Activityquery } from "../Apollo/queries";
+
+import { Auth } from "aws-amplify";
+
 import { Select } from "antd";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import TermsConditions from "../Components/TermsConditions";
-import AthleteFeedback from "../Components/AthleteFeedback";
+
 import ThirdPartyCard from "../Components/ThirdPartyCard";
-import AthleteCard from "../Components/AthleteCard";
-import ActivityCard from "../Components/ActivityCard";
-import Header from "../Components/Header";
+
 const { Option } = Select;
 
-function secondsToHms(d) {
-  d = Number(d);
-  var h = Math.floor(d / 3600);
-  var m = Math.floor((d % 3600) / 60);
-  var s = Math.floor((d % 3600) % 60);
-
-  var hDisplay = h > 0 ? h + (h == 1 ? ":" : ":") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? ":" : ":") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? ":" : "") : "";
-  return hDisplay + mDisplay + sDisplay;
-}
-
-function MinPerKmFraction(MinPerKm, GarminActivityType) {
-  switch (GarminActivityType) {
-    case "LAP_SWIMMING":
-      MinPerKm = Number(MinPerKm);
-      var SecPerHundred = (MinPerKm / 10) * 60;
-      var Mins = Math.floor(SecPerHundred / 60);
-      var Secs = Math.floor(SecPerHundred - Mins * 60);
-      return Mins + ":" + Secs;
-    case "STRENGTH_TRAINING":
-      return "-";
-    case "RUNNING": {
-      MinPerKm = Number(MinPerKm);
-      var mins = Math.floor(MinPerKm / 1);
-      var fraction = Math.floor((MinPerKm - mins) * 60);
-      return mins + ":" + fraction;
-    }
-    case "CYCLING":
-      MinPerKm = Number(MinPerKm);
-      var KmPerHr = (1 / MinPerKm) * 60;
-      return KmPerHr.toFixed(2);
-    case "VIRTUAL_RIDE":
-      MinPerKm = Number(MinPerKm);
-      var KmPerHr = (1 / MinPerKm) * 60;
-      return KmPerHr.toFixed(2);
-    default:
-      return "-";
-  }
-}
-
 function ThirdParty() {
-  const [activities, setActivities] = React.useState([]);
-
   const [userId, setUserId] = useState("");
   // const { loading, error, data } = useQuery(firstQuery)
 
@@ -94,44 +30,19 @@ function ThirdParty() {
       .catch((err) => console.log(err));
   }, []);
 
-  const sortDesByDate = (a, b) => {
-    if (new Date(a.updatedAt) > new Date(b.updatedAt)) {
-      return -1;
-    }
-    if (new Date(a.updatedAt) < new Date(b.updatedAt)) {
-      return 1;
-    }
-    return 0;
-  };
-  async function fetchActivities() {
-    try {
-      const activity = await API.graphql(graphqlOperation(Activityquery));
-      console.log("Activity items returned:" + activity.data.activitiesgarminByGarminAccountId.items);
-      let sorted =
-        activity.data.activitiesgarminByGarminAccountId.items.sort(
-          sortDesByDate
-        );
-      sorted = sorted.filter(
-        (exr) =>
-          !exr.GarminActivityAthleteFeedback ||
-          exr.GarminActivityAthleteFeedback != 1
-      );
-      setActivities(sorted.slice(0, 10));
-    } catch (err) {
-      console.log("Error fetching activities");
-    }
-  }
-  useEffect(() => {
-    fetchActivities();
-  }, []);
   return (
     <div>
       {/* <Header user={userId} /> */}
       <div className="bodyDiv">
         <Row>
-          <Col className="firstCol" span={8} xs={24} sm={24} lg={8} xl={8}>
-           
-          </Col>
+          <Col
+            className="firstCol"
+            span={8}
+            xs={24}
+            sm={24}
+            lg={8}
+            xl={8}
+          ></Col>
           <Col className="secondCol" span={8} xs={24} sm={24} lg={8} xl={8}>
             <h1
               style={{
@@ -144,36 +55,11 @@ function ThirdParty() {
             </h1>
 
             <div className="cardSpacingDiv">
-                    <ThirdPartyCard userId={userId} />
-                  </div>
-                  </Col>
-
-//           {activities.map(
-//              ({
-//                id,
-//                GarminActivityId,
-  //              GarminActivityType,
-  //              GarminActivityDescription,
-  //              GarminAveragePaceInMinutesPerKilometer,
-  //              GarminActivityStartTime,
-  //              GarminActivityDuration,
-  //              GarminActivityDistance,
-  //              GarminAverageHeartRateInBeatsPerMinute,
-  //              GarminActivityAthleteBody,
- //               GarminActivityAthleteEffort,
- //               _version
- //             }) => {
- //               return (
- //                 <div key={id} className="cardSpacingDiv">
-  //                  <ThirdPartyCard userId={userId} />
- //                 </div>
- //               );
- //             }
-//            )}
-
+              <ThirdPartyCard userId={userId} />
+            </div>
+          </Col>
 
           <Col className="thirdCol" span={8} xs={24} sm={24}>
-          
             <div
               style={{
                 marginRight: "40px",
@@ -181,7 +67,6 @@ function ThirdParty() {
                 marginLeft: "40px",
               }}
             ></div>
-           
           </Col>
         </Row>
       </div>
