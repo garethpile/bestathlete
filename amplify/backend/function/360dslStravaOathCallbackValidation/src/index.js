392ebdd5c47aca860ae2606cd8d948f00b1d40f6
Gareth Pile <pile.gareth@gmail.com>
2023-07-22 08:37:19 +0200

    Return success

--- amplify/backend/function/360dslStravaOathCallbackValidation/src/index.js
+++ amplify/backend/function/360dslStravaOathCallbackValidation/src/index.js
@@ -60,5 +60,22 @@ exports.handler = async (event) => {
       return response;
     }
   } else {
+    var bodyData = {
+      "Response": "Success",
+    };
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,",
+      },
+      body: JSON.stringify(bodyData),
+    };
+
+    console.log(response);
+
+    return response;
   }
 };
